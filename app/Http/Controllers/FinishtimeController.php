bf82e29b8e6551b9241fdd8fc4096f2c872df625
yantao <a1292717155@gmail.com>
2016-07-04 16:01:32 +0200

    a119347

--- app/Http/Controllers/FinishtimeController.php
+++ app/Http/Controllers/FinishtimeController.php
@@ -12,22 +12,22 @@ class FinishtimeController extends Controller
     //
     public function store(Request $request)
     {
-        if(Finishtime::where('price_table_list_id',$request->get('price_table_list_id'))->first())
+        if(FinishTime::where('price_table_list_id',$request->get('price_table_list_id'))->first())
         {
-            $data=Finishtime::where('price_table_list_id',$request->get('price_table_list_id'))->first();
+            $data=FinishTime::where('price_table_list_id',$request->get('price_table_list_id'))->first();
             $data->time1=$request->get('time1');
             $data->time2=$request->get('time2');
             $data->time3=$request->get('time3');
             $data->save();
 
         } else {
-            Finishtime::create($request->all());
+            FinishTime::create($request->all());
         }
         return redirect()->back();
     }
 
     public function index($id)
     {
-        return Finishtime::where('price_table_list_id',$id)->first();
+        return FinishTime::where('price_table_list_id',$id)->first();
     }
 }
