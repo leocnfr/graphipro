91f275d2b38f6051f696a5d47310c61566b65ee4
yantao <a1292717155@gmail.com>
2016-07-06 14:04:25 +0200

    a119347

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -5,12 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Storage;
 
 class PaymentController extends Controller
 {
     //
     public function payment(Request $request)
     {
-        dd($request->all());
+        Storage::disk('local')->put('file.txt', 'Contents');
+        $files=$request->file('files');
+        foreach ($files as $file) {
+            Storage::put('files/'.
+                $file->getClientOriginalName(),
+                file_get_contents($file->getRealPath())
+            );
+        }
+
     }
 }
