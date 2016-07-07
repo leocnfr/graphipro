04454ee79ba222e2361228380199a1faa68078e5
yantao <a1292717155@gmail.com>
2016-07-07 18:13:47 +0200

    a119347

--- resources/views/graphipro/special.blade.php
+++ resources/views/graphipro/special.blade.php
@@ -17,8 +17,8 @@
                 </select>
                 <br/>
                 <input type="hidden" value="" name="price" id="price"/>
-                <input type="hidden" value="<?=$id?>" name="id"/>
-                <input type="hidden" value="<?=$name?>" name="pname"/>
+                <input type="hidden" value="" name="id"/>
+                <input type="hidden" value="" name="pname"/>
             </div>
             <div style="font-size:20px; color:#29ABE2; margin-top:20px;">2. Option de livraison</div><br />
             <div style="padding:10px; width:400px; border-radius:3px; background-color:#F2F2F2;">
