df97405681fa94dc3efb074e7919286980152453
yantao <a1292717155@gmail.com>
2016-07-07 17:43:16 +0200

    a119347

--- resources/views/graphipro/produit_template.blade.php
+++ resources/views/graphipro/produit_template.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div style="width: 100%">
     <div style="width:1000px;margin:0px auto; margin-top:20px; color:#333">
-        <a href="index.php" style="color:#666">Accueil</a> > <span id="pro_name">{{$product->name}}</span><br />
+        <a href="{{url('/')}}" style="color:#666">Accueil</a> > <span id="pro_name">{{$product->name}}</span><br />
         <div style="margin-top:20px; color:#29ABE2; font-size:30px;">{{$product->name}}</div>
         <div style="float:left; margin-top:20px; width:250px; height:250px; overflow:hidden; border:thin ridge #29ABE2; background-color:#FFF;"><center><img id="proimg" src="/storage/uploads/{{$product->productimg}}" width="250"/></center></div>
         <div style="float:left; margin-left:40px; margin-top:30px; font-size:30px; color:#29ABE2;">
