064cf7da7987a0ec0fa54acf2e40ee8634f4364d
yantao <a1292717155@gmail.com>
2016-07-07 10:40:18 +0200

    a119347

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in! it
+                    You are logged in! it 123
                 </div>
             </div>
         </div>
