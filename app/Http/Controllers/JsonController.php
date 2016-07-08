6a4df84ba1f64a98e698ccdf3131d32a33b2ff11
yantao <a1292717155@gmail.com>
2016-07-08 17:11:29 +0200

    a119347

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -51,7 +51,7 @@ class JsonController extends Controller
             ->get();
         $imprimers=array();
         foreach ($tables as $table) {
-            $imprimers= array_merge($table->imprimers,$imprimers);
+            $imprimers= array_push($table->imprimers,$imprimers);
         }
         $imprimers=array_unique($imprimers);
         dd($imprimers);
