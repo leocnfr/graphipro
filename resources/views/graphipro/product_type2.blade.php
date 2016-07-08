a04002381546277e8331ecdc1fa006b584b3fda0
yantao <a1292717155@gmail.com>
2016-07-08 10:46:17 +0200

    a119347

--- resources/views/graphipro/product_type2.blade.php
+++ resources/views/graphipro/product_type2.blade.php
@@ -171,13 +171,13 @@
                 if (this.m2<=0.3){
                     var val=(0.5*this.quantity*parseInt(this.materiels.price)*1.2).toFixed(2);
                     $('#price').val(val);
-                    $('#showprice').val(val);
+                    $('#showprice').html(val);
                     console.log(val);
                     return val;
                 }else {
                     var val=(this.quantity*this.m2*parseInt(this.materiels.price)*1.2).toFixed(2);
                     $('#price').val(val);
-                    $('#showprice').val(val);
+                    $('#showprice').html(val);
                     console.log(val);
                     return val;
                 }
