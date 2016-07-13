f6d5f13ae00207407b8f0540663a094d53a4db0f
yantao <a1292717155@gmail.com>
2016-07-13 17:26:26 +0200

    yantao

--- resources/views/admin/order/show.blade.php
+++ resources/views/admin/order/show.blade.php
@@ -6,6 +6,7 @@
             <th></th>
             <th>客户</th>
             <th>内容</th>
+            <th>数量</th>
             <th>价格</th>
             <th>创建时间</th>
         </tr>
@@ -16,6 +17,7 @@
                 <td></td>
                 <td>{{$order->belongsUser->nom}}</td>
                 <td>{!! $order->content !!}</td>
+                <td>{{$order->ex}}</td>
                 <td>{{$order->price}}</td>
                 <td> {{$order->created_at}} <br>
                     {{$order->created_at->diffForHumans()}}
