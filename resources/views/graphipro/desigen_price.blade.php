3f4dc66eee1c709813e7d60ba80e820f71212461
yantao <a1292717155@gmail.com>
2016-07-13 12:35:45 +0200

    111

--- resources/views/graphipro/desigen_price.blade.php
+++ resources/views/graphipro/desigen_price.blade.php
@@ -1,5 +1,5 @@
 <span>Création:</span>
-
+<br>
 <select id="creation" name="jumpMenu" id="jumpMenu" style="width:220px;">
     <option value="0">Fournir par client</option>
     <option value="{{$product->design_price}}">+{{$product->design_price}}</option>
