b87d93717b870d0f6a31f96b0d1cc1d234832816
yantao <a1292717155@gmail.com>
2016-07-15 14:28:02 +0200

    yantao

--- app/Products.php
+++ app/Products.php
@@ -35,7 +35,7 @@ class Products extends Model
 
     public function hasOnePro($proid)
     {
-        dd(Promotion::where('product_id','=',$proid)->first()->toArray()->count);
+        dd(Promotion::where('product_id','=',$proid)->first()->toArray());
         return Promotion::where('product_id','=',$proid)->first()->toArray();
     }
 }
