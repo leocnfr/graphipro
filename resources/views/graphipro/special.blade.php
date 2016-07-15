5ae2cef83a554611bcf59ae7dbc9bbeeb4a0da99
yantao <a1292717155@gmail.com>
2016-07-15 12:12:54 +0200

    yantao

--- resources/views/graphipro/special.blade.php
+++ resources/views/graphipro/special.blade.php
@@ -15,9 +15,6 @@
                 <br/>
                @include('graphipro.desigen_price')
                 <br/>
-                <input type="hidden" value="" name="price" id="price"/>
-                <input type="hidden" value="" name="id"/>
-                <input type="hidden" value="" name="pname"/>
             </div>
             <div style="font-size:20px; color:#29ABE2; margin-top:20px;">2. Option de livraison</div><br />
             <div style="padding:10px; width:400px; border-radius:3px; background-color:#F2F2F2;">
@@ -31,3 +28,7 @@
         </td>
     </tr>
 </table>
+<script>
+    var price=$('#quantity').val()*$('#formate').val();
+    $('#showprice').html(parseInt(price).toFixed(2));
+</script>
