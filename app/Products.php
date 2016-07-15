4129a004a25c86a0d71088f6191bbec7a702a870
yantao <a1292717155@gmail.com>
2016-07-15 14:20:45 +0200

    yantao

--- app/Products.php
+++ app/Products.php
@@ -35,6 +35,6 @@ class Products extends Model
 
     public function hasOnePro($proid)
     {
-        return Promotion::where('product_id','=',$proid)->first()->toJson();
+        return Promotion::where('product_id','=',$proid)->first();
     }
 }
