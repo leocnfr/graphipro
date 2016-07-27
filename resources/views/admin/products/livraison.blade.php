f7514e203036c1407fc6ef92cf6005ab16f2482e
yantao <a1292717155@gmail.com>
2016-07-27 15:28:43 +0200

    yan

--- resources/views/admin/products/livraison.blade.php
+++ resources/views/admin/products/livraison.blade.php
@@ -1,28 +1,28 @@
 <h3>运输价格</h3>
 <div class="row">
 	<div class="col-md-4">
-		<form action="/admin/livraison" method="post" role="form">
-			<input type="hidden" value="{{$product->id}}" name="product_id">
+		{{--<form action="/admin/livraison" method="post" role="form">--}}
+			<input type="hidden" value="{{$product->id}}" name="product_id" v-model="product_id">
 			<div class="radio">
 				<label>
-					<input type="radio" name="postcode" id="inputID" value="1" checked="checked">75
+					<input type="radio" name="postcode" id="inputID" value="1" checked="checked" v-model="postcode">75
 				</label>
 			</div>
 			<div class="radio">
 				<label>
-					<input type="radio" name="postcode" id="inputID" value="2" >75以外
+					<input type="radio" name="postcode" id="inputID" value="2" v-model="postcode">75以外
 				</label>
 			</div>
 			<div class="form-group">
 				<label for=""></label>
-				<input type="text" class="form-control" name="numbers" id="" placeholder="数量或平米数" required>
+				<input type="text" class="form-control" name="numbers" id="" placeholder="数量或平米数" v-model="numbers" required>
 			</div>
 			<div class="form-group">
 				<label for=""></label>
-				<input type="number" step="0.01" class="form-control" name="price" id="" placeholder="价格" required>
+				<input type="number" step="0.01" class="form-control" name="price" id="" placeholder="价格" v-model="price" required>
 			</div>
-			<button type="submit" class="btn btn-primary">Submit</button>
-		</form>
+			<button type="submit" class="btn btn-primary" @click="store()">Submit</button>
+		{{--</form>--}}
 	</div>
 	<div class="col-md-8">
 		<table class="table table-hover">
@@ -34,20 +34,27 @@
 			</tr>
 			</thead>
 			<tbody>
-			@foreach($livrasions as $livrasion)
-				<tr>
-					<td>{{$livrasion->postcode==1?75:'75以外'}}</td>
-					<td>{{$livrasion->numbers}}</td>
-					<td>{{$livrasion->price}}</td>
-					<td>
-						<form action="/admin/livraison/{{$livrasion->id}}" method="post">
-							{{method_field('DELETE')}}
-							<button onclick="return confirm('确定删除?')" class="btn btn-danger">删除</button>
-						</form>
-					</td>
+				<tr v-for="result in results">
+					<td>@{{ result.postcode }}</td>
+					<td>@{{ result.numbers }}</td>
+					<td>@{{ result.price }}</td>
+					<td><button class="btn btn-danger" @click='delete(result)'>删除</button></td>
 				</tr>
-			@endforeach
+			{{--@foreach($livrasions as $livrasion)--}}
+				{{--<tr>--}}
+					{{--<td>{{$livrasion->postcode==1?75:'75以外'}}</td>--}}
+					{{--<td>{{$livrasion->numbers}}</td>--}}
+					{{--<td>{{$livrasion->price}}</td>--}}
+					{{--<td>--}}
+						{{--<form action="/admin/livraison/{{$livrasion->id}}" method="post">--}}
+							{{--{{method_field('DELETE')}}--}}
+							{{--<button onclick="return confirm('确定删除?')" class="btn btn-danger">删除</button>--}}
+						{{--</form>--}}
+					{{--</td>--}}
+				{{--</tr>--}}
+			{{--@endforeach--}}
 			</tbody>
 		</table>
 	</div>
 </div>
+<script src="/js/livrasion.js"></script>
