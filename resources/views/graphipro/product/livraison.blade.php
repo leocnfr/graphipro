9365330efbb8f92083d60905342c2edb653faa8a
yantao <a1292717155@gmail.com>
2016-07-27 10:33:15 +0200

    yan

--- resources/views/graphipro/product/livraison.blade.php
+++ resources/views/graphipro/product/livraison.blade.php
@@ -1,8 +1,37 @@
+<style>
+    #new-address input{
+        margin: 5px 0;
+    }
+</style>
 <div style="font-size:20px; color:#29ABE2; margin-top:20px;">2. Option de livraison</div>
 <br/>
 <div style="padding:10px; width:400px; border-radius:3px; background-color:#F2F2F2;">
    Votre address:
+    <br>
     @if(Auth::check())
-    <input type="radio" name="livraison_address" id="" value="" checked="checked">{{Auth::user()->address}}
+    <input type="radio" name="livraison_address" id="" value="1" checked="checked">{{Auth::user()->address}}
+        <br>
     @endif
+
+    <input type="radio" name="livraison_address" id="new" value="2">New address
+        <div id="new-address" style="display: none">
+            <input type="number" minlength="5" placeholder="votre post code" name="postcode"> <br>
+            <input type="text" placeholder="votre ville" name="ville"> <br>
+            <input type="text" placeholder="votre address" name="address">
+        </div>
+
 </div>
+<script>
+    $( "input[name$='livraison_address']" ).change(function () {
+       showNewAddress();
+    });
+    function showNewAddress() {
+        var liv_address= $('input[name="livraison_address"]:checked ').val();
+        if (liv_address==2){
+            $('#new-address').show();
+        }else {
+            $('#new-address').hide();
+        }
+    }
+
+</script>
